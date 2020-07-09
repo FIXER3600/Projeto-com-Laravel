2a458e2c5ff2e4ed24da8aa0d9a3cd1821670a18
FIXER3600 <guilhermecomercial51@gmail.com>
2020-07-09 20:21:10 -0300

    alteracaobanco

--- routes/web.php
+++ routes/web.php
@@ -13,18 +13,17 @@ use Illuminate\Support\Facades\Route; use App\User;
 |
 */
 
-Route::get('/', function () {
-     return view('welcome'); 
-    });
+Route::get('/', function () { return view('welcome'); });
 
-Route::get('/userlist', function () {
-
+Route::get('/userlist', 
+function () {
+for($contador = 0; $contador < 50; $contador++){
 $umUsuarioQualquer = new User;
-$umUsuarioQualquer->name = 'aUserName';
+$umUsuarioQualquer->name = 'umUser'.$contador;
 $timestamp = date("Y-m-d-h-i-sa");
-$umUsuarioQualquer->email = "adsf@asdf.com".$timestamp;
+$umUsuarioQualquer->email = "email".$contador."@emailTeste.com".$timestamp;
 $umUsuarioQualquer->password = '123';
-$umUsuarioQualquer->save(); 
+$umUsuarioQualquer->save(); }
 return User::all(); 
 });
 
